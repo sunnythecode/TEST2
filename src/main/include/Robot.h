98298b7eaa9d84ab67c377d30df14c920233e8d8
Sandeep Bajamahal <sandeep@Sandeeps-MBP.hsd1.ca.comcast.net>
2022-01-29 09:14:59 -0800

    FR

---
+++ src/main/include/Robot.h
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+#include "SDrive.h"
+#include <frc/TimedRobot.h>
+#include <frc2/command/Command.h>
+#include <frc/Joystick.h>
+#include "RobotContainer.h"
+#include <rev/CANSparkMax.h>
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void TestPeriodic() override;
+  SDrive drive;
+
+
+
+ private:
+  // Have it null by default so that if testing teleop it
+  // doesn't have undefined behavior and potentially crash.
+  frc2::Command* m_autonomousCommand = nullptr;
+
+  RobotContainer m_container;
+};
+
---
+++ src/main/include/SDrive.h
@@ -0,0 +1,22 @@
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/Joystick.h>
+#include <rev/CANSparkMax.h>
+
+
+class SDrive
+{
+private:
+
+
+public:
+    #include <frc/smartdashboard/SmartDashboard.h>
+    #include <frc/Joystick.h>
+    #include <rev/CANSparkMax.h>
+    frc::Joystick jstick{0};
+    rev::CANSparkMax *leftMotor;
+    rev::CANSparkMax *rightMotor;
+
+
+
+
+};
